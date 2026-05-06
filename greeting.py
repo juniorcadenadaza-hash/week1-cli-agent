7e36a550db917c3cd401789d5f850da32e517851
Junior Cadena <juniorcadenadaza@gmail.com>
2026-05-06 16:55:40 -0500

    Enhanced greeting with age and hobby

--- greeting.py
+++ greeting.py
@@ -1,3 +1,7 @@
 # my first python automation script!
 name = input("What is your name? ")
 print("Hello " + name + "! Welcome to automation!")
+Age = input("How old are you? ")
+print("Wow, " + Age + " is a great age to start learning automation!")
+favorite_hobby = input("What is your favorite hobby? ")
+print("That's great! " + favorite_hobby + " is a fun hobby to have!")
